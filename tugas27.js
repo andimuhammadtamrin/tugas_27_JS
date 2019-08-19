aa0aa8ffe31be3aadef15c71ad6da329db444a1d
Andi Muhammad Tamirn <andimuhammadtamrin@gamil.com>
2019-08-19 22:57:39 +0800

    Tugas 27 Java Script
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas27.js
@@ -0,0 +1,10 @@
+var a = "No ";
+var b = "rumah saya ";
+var c = 30;
+var d = " A";
+var e = "Jalan pangera Tirtayasa No ";
+var f = 70;
+hasil1 = a+b+c+d;
+console.log(hasil1);
+hasil2 = e+f;
+console.log(hasil2);
